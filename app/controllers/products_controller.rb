c0577294a23c7ad0928ea497e84c9d393eff2dab
Luu Kha <luu.tuan.kha@framgia.com>
2016-07-13 17:49:27 +0700

    select

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,7 +2,13 @@ class ProductsController < ApplicationController
   before_action :logged_in_user
 
   def index
-    @products = Product.paginate page: params[:page]
+    load_products
+    @selected_classify = params[:classify]
+    @selected_category = params[:category]
+    @selected_sorted_by = params[:sorted_by]
+    @products = Product.filter @selected_classify, @selected_category, 
+      @selected_sorted_by
+    @products = @products.paginate page: params[:page], per_page: 6   
   end
 
   def show
@@ -13,5 +19,18 @@ class ProductsController < ApplicationController
     end 
     @comments = @product.comments.paginate page: params[:page]
     @comment ||= @product.comments.new
+  end
+
+  private
+  def load_products
+    @categories = Category.all
+    @classify_selects =
+      Product.classifies.map{|key, value| [I18n.t("classify.#{key}"), value]}
+    @sorted_by_selects = [[I18n.t("product.sort_by_Aphabet"), "name DESC"], 
+      [I18n.t("product.sort_by_sprice_asc"), "price ASC"], 
+      [I18n.t("product.sort_by_sprice_desc"), "price DESC"], 
+      [I18n.t("product.sort_by_rating_asc"), "rating ASC"], 
+      [I18n.t("product.sort_by_rating_desc"), "rating DESC"]] 
+    @category_selects = @categories.collect{|cat| [cat.name, cat.id]}
   end
 end
